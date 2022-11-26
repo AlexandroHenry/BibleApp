204ac8058ee2efd931e4dc342a0a21073077d615
alaxhenry <alaxhenry0121@gmail.com>
2022-11-26 16:09:01 +0900

    Initial Commit

---
+++ BibleApp/BibleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BibleAppApp.swift
+//  BibleApp
+//
+//  Created by Seungchul Ha on 2022/11/26.
+//
+
+import SwiftUI
+
+@main
+struct BibleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
